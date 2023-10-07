29131b136d36ee838ca683a9937aa226e9ae3d28
Tatenda Bako <63616801+tatendabakozw@users.noreply.github.com>
2023-10-06 21:25:51 -0700

    for render test

---
+++ apps/locations-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'locations-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/locations-api',
+};
