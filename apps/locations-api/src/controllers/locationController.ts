a60ad448e3d7d8aa97d5decdaa6ecba4fa15ab6e
tatendabakozw <tatendabakozw@gmail.com>
2023-10-19 17:08:30 +0200

    feaure: added locations controllers

--- apps/locations-api/src/controllers/locationController.ts
+++ apps/locations-api/src/controllers/locationController.ts
@@ -14,3 +14,49 @@ export const addNewLocation = async (
     next(error);
   }
 };
+
+// get bus stops near  locaation
+// get request
+// /api/bus-stop/near/?lon=lon&lat=lat
+export const getNearBusStop = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log('add a bustop');
+  } catch (error) {
+    next(error);
+  }
+};
+
+// edit location
+// patcch requeest
+// /api/bus-stop/edit/?bus_stop_id=_id
+export const editLocation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      console.log('add a bustop');
+    } catch (error) {
+      next(error);
+    }
+  };
+
+// delete location
+// delete request
+// /api/bus-stop/delete/?bus_stop_id=_id
+export const deleteLocation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      console.log('add a bustop');
+    } catch (error) {
+      next(error);
+    }
+  };
+
--- apps/locations-api/src/routes/bus-stop/bus-stop.ts
+++ apps/locations-api/src/routes/bus-stop/bus-stop.ts
@@ -1,49 +1,30 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { addNewLocation } from '../../controllers/locationController';
+import express from 'express';
+import {
+  addNewLocation,
+  deleteLocation,
+  editLocation,
+  getNearBusStop,
+} from '../../controllers/locationController';
 const router = express();
 
 // add a bus stop
 // post request
 // /api/bus-stop/add
-router.post('/add',addNewLocation);
+router.post('/add', addNewLocation);
 
 // get bus stops near  locaation
 // get request
 // /api/bus-stop/near/?lon=lon&lat=lat
-router.get('/near', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    console.log('add a bustop');
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/near', getNearBusStop);
 
 // edit location
 // patcch requeest
 // /api/bus-stop/edit/?bus_stop_id=_id
-router.patch(
-  '/edit',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      console.log('location items');
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+router.patch('/edit', editLocation);
 
 // delete location
 // delete request
 // /api/bus-stop/delete/?bus_stop_id=_id
-router.delete(
-  '/delete',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      console.log('delete a route');
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+router.delete('/delete', deleteLocation);
 
-export default router
+export default router;
